a4470f410baea60e7a8254c82b68fa9ca91b98c6
Harsh Vardhan Jha <harsh.jha@attentive.ai>
2026-03-27 18:54:10 +0530

    Context-part-1

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Test from './component/Test';
 import Todo from './component/Todo';
+import UserContextProvider from './context/UserContextProvider';
+import Login from './component/Login';
+import Profile from './component/Profile';
 
 
 function App() {
   return (
-    <>
-   <BrowserRouter>
+    <UserContextProvider>
+   {/* <BrowserRouter>
       <Link to="/todo">Go to Todo Page</Link>
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/test" element={<Test />} />
         <Route path="/todo" element={<Todo/>}/>
       </Routes>
-    </BrowserRouter>
-    </>
+    </BrowserRouter> */}
+    <h1>React with chai and code </h1>
+    <Login/>
+    <Profile/>
+    </UserContextProvider>
   )
 }
 
